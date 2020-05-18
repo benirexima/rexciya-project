ae1b85a05f144a6f5ea29a13ab6454bd5c123e77
benirexima <52735980+benirexima@users.noreply.github.com>
2020-05-18 10:01:12 +0530

    Add files via upload

---
+++ medical/src/main/java/com/project/service/UserService.java
@@ -0,0 +1,12 @@
+package com.project.service;
+
+import com.project.model.User;
+
+public interface UserService {
+	
+
+	 public User findUserByEmail(String email);
+	 
+	 public void saveUser(User user);
+
+}
